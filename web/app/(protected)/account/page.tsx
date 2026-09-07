a9735b9869fddfb383d1f83c286a96a344d1e030
pizmiz1 <ebritton12321@gmail.com>
2026-09-07 18:19:33 -0500

    Remove unneeded delay

--- web/app/(protected)/account/page.tsx
+++ web/app/(protected)/account/page.tsx
@@ -4,10 +4,6 @@ import { getUserSettings } from "@/lib/helpers/common-fetch";
 const AccountPage = async () => {
   const userSettings = await getUserSettings();
 
-  await new Promise((resolve) => {
-    setTimeout(resolve, 3000);
-  });
-
   return <Content userSettings={userSettings} />;
 };
 
